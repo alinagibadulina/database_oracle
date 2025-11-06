f6c4b35c0d89e6048e8684c69be940f290f893cc
Alina Gibadulina <153265125+alinagibadulina@users.noreply.github.com>
2025-11-06 15:14:42 +0300

    Add files via upload

---
+++ лабы/test.sql
@@ -0,0 +1,14 @@
+--1
+select * from dba_views;
+--2
+select * from dba_tablespaces;
+--3
+select * from dba_users;
+--4
+select * from dba_segments;
+--5
+select * from cdb_pdbs;
+--9
+select * from v$session;
+--10
+select * from v$bgprocess;
